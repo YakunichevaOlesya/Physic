eb9e0b979b2d9af613d1bc945a49cfabfd005686
YakunichevaOlesya <78850282+YakunichevaOlesya@users.noreply.github.com>
2021-05-27 11:06:30 +0300

    Update main (7).cpp

--- main (7).cpp
+++ main (7).cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <cmath>
 
-const double g = 9.81;
+const long double M = 5.9736 * 1e24;
+const long long int R = 6371000;
+const long double G = 6.6743 * 1e-11;
 
 class Rocket {
 private:
@@ -23,20 +25,12 @@ public:
         return x0 + t * t * (fuelBurnRate * gasFlowRate / (m + fuel - fuelBurnRate * t)) * cos(alpha) / 2;
     }
 
-    double yCoordinate(double t) {
-        return y0 + t * t * ((fuelBurnRate * gasFlowRate / (m + fuel - fuelBurnRate * t)) * sin(alpha) - g) / 2;
-    }
-
     double speedX(double t) {
         return fuelBurnRate * gasFlowRate / (m + fuel - fuelBurnRate * t) * cos(alpha) * t;
     }
 
-    double speedY(double t) {
-        return ((fuelBurnRate * gasFlowRate / (m + fuel - fuelBurnRate * t)) * sin(alpha) - g) * t;
-    }
-
-    double speedModule(double t) {
-        return fabs(((fuelBurnRate * gasFlowRate / (m + fuel - fuelBurnRate * t)) - g) * t);
+    double speedModule(double t, double curG) {
+        return fabs(((fuelBurnRate * gasFlowRate / (m + fuel - fuelBurnRate * t)) - curG * sin(alpha)));
     }
 
     double fuelMass(double t) {
@@ -50,16 +44,23 @@ int main() {
     Rocket r(x0, y0, alpha, m, fuel, gasFlowRate, fuelBurnRate);
     double t = 0;
     FILE *file = fopen("data.txt", "w");
+    double curY = y0, curM, curFuel, curSpeedY;
+    long double curG;
     while (true) {
         std::cout << "Current time: " << t << " seconds from the start.\n";
+        curG = G * M / pow(R + curY, 2);
+        curFuel = r.fuelMass(t);
+        curM = m + curFuel;
+        curY += ((fuelBurnRate * gasFlowRate / (curM)) * sin(alpha) - curG) / 2;
+        curSpeedY = ((fuelBurnRate * gasFlowRate / (curM)) * sin(alpha) - curG) * t;
         std::cout << "Current x-axis coordinate: " << r.xCoordinate(t) << '\n';
-        std::cout << "Current y-axis coordinate: " << r.yCoordinate(t) << '\n';
+        std::cout << "Current y-axis coordinate: " << curY << '\n';
         std::cout << "Current x-axis speed: " << r.speedX(t) << '\n';
-        std::cout << "Current y-axis speed: " << r.speedY(t) << '\n';
-        std::cout << "Current speed: " << r.speedModule(t) << '\n';
-        std::cout << "Current fuel mass: " << r.fuelMass(t) << "\n\n";
-        fprintf(file, "%f\t%f\n", r.xCoordinate(t), r.yCoordinate(t));
-        if (r.fuelMass(t) == 0)
+        std::cout << "Current y-axis speed: " << curSpeedY << '\n';
+        std::cout << "Current speed: " << r.speedModule(t, curG) << '\n';
+        std::cout << "Current fuel mass: " << curFuel<< "\n\n";
+        fprintf(file, "%f\t%f\n", r.xCoordinate(t), curY);
+        if (curFuel == 0)
             break;
         t++;
     }
